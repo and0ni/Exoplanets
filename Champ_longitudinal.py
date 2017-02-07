24dcec8ddabe7fa6b5fb1c5e25a91ebb8c2e4a49
and0ni <Andoni.Torres-Rivas@USherbrooke.ca>
2017-02-06 16:28:59 -1000

    Update Champ_longitudinal.py

--- Champ_longitudinal.py
+++ Champ_longitudinal.py
@@ -1,83 +1,79 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 17 2017
-@author: Andoni Torres
-Programme qui lit les données de graphiques lsd et délimite les bornes d'intégration selon le fit gaussien de Stokes I et calcule
-l'intrégrale entre ces bornes des paramètres de Stokes afin de calculer le champ longitudinal Bl et Nl
-"""
-
+"""Created on Tue Jan 17 2017 @author: Andoni Torres Programme qui lit les données de graphiques lsd et délimite les
+bornes d'intégration selon le fit gaussien de Stokes I et calcule l'intrégrale entre ces bornes des paramètres de
+Stokes afin de calculer le champ longitudinal Bl et Nl """
 
 import numpy as np
-import os 
+import os
 import glob
 from scipy.integrate import simps
 from scipy.optimize import curve_fit
 
-os.chdir("/data/coolsnap/data_coolsnap_archive")
-#os.chdir("/data/coolsnap/spectra_espadons_coolsnap/")
 
-def gaus(x,a,x0,sigma,ic):
-    return ic-a*np.exp(-(x-x0)**2/(2*sigma**2))
+def gaus(x, a, x0, sigma, ic):
+    return ic - a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
 
-def integ(a,b):
-    return float(simps(a,b))
+
+def integ(a, b):
+    return float(simps(a, b))
+
 
 liste = []
-atrocites = [] # Liste des fichiers dont le graphique Stokes I ne représentent pas une gausienne
+atrocites = []  # Liste des fichiers dont le graphique Stokes I ne représentent pas une gausienne
 cc = 299792
-compteur = 0
 
-for file in glob.glob("[0-9]*pn.lsd"):
-   liste.append(file)
+# for file in glob.glob("/data/coolsnap/data_coolsnap_archive/[0-9]*pn.lsd"):
+for file in glob.glob("/data/coolsnap/spectra_espadons_coolsnap/[0-9]*pn.lsd"):
+    liste.append(file)
 
-wave_lande = np.genfromtxt('/h/torres/Downloads/mean_wave_lande(true)',skip_header=0, dtype=[('0','S15'),('1','f8'),('2','f8')])
+wave_lande = np.genfromtxt('/h/torres/Downloads/mean_wave_lande(true)', dtype=[('0', 'S15'), ('1', 'f8'), ('2', 'f8')])
 
-noms = wave_lande['0'] 
+noms = wave_lande['0']
 wave = wave_lande['1']
 lande = wave_lande['2']
 
 """# Lire les noms des fichiers a partir d'une liste
 liste = np.genfromtxt('/h/torres/toto',dtype=str)"""
 
-print ('Fichier', 'Bl', 'erreur', 'x0',	'sigma', 'Ic',	'Hauteur du pic')
+print ('Fichier', 'Bl', 'erreur', 'x0', 'sigma', 'Ic', 'Hauteur du pic')
 
 for filename in liste:
 
-    for i in range(0,(len(noms))):
+    for i in range(0, (len(noms))):
         if filename == noms[i]:
             geff = lande[i]
             lambda0 = wave[i]
         else:
             geff = 1.232
             lambda0 = 668.1893
-	coeff = (-2.14*10**11)/(lambda0*cc*geff)
+        coeff = (-2.14 * 10 ** 11) / (lambda0 * cc * geff)
 
-#    os.chdir("/data/coolsnap/data_coolsnap_archive")
-    A = np.genfromtxt(filename,skip_header=2, dtype=[('0','f8'),('1','f8'),('2','f8'),('3','f8'),('4','f8'),('5','f8'),('6','f8')])
-    
-# Defnir les donnes
-# Vitesse radiale
-    VR = A['0']
-# Stokes I
-    SI = A['1']
-# Stokes V
-    SV = A['3']
-# Stokes N
-    SN = A['5']
-    
-# Erreurs sur les données
+    #    os.chdir("/data/coolsnap/data_coolsnap_archive")
+    A = np.genfromtxt(filename, skip_header=2,
+                      dtype=[('0', 'f8'), ('1', 'f8'), ('2', 'f8'), ('3', 'f8'), ('4', 'f8'), ('5', 'f8'), ('6', 'f8')])
+
+    # Defnir les donnes
+
+    VR = A['0']  # Vitesse radiale
+    SI = A['1']  # Stokes I
+    SV = A['3']  # Stokes V
+    SN = A['5']  # Stokes N
+
+    # Erreurs sur les données
     eSI = A['2']
     eSV = A['4']
     eSN = A['6']
-       
-# Lissage gaussien (permet de définir les bornes d'intégration)
-      
-    Xmoy = np.mean(VR[np.where(SI == min(SI))]) # Abcisse du SI minimum
-    
-    params0=[0.2, Xmoy, 5.0, 1.0]  # Estimation initiale des paramètres
-    
-    popt, pcov = curve_fit(gaus, VR, SI, params0) # popt[]:paramètres optimisés; popt[0]: ordonnée moyenne ,popt[1]: abscisse moyenne, popt[2]:sigma, popt[3]: Icontinu
-    
+
+    # Lissage gaussien (permet de définir les bornes d'intégration)
+
+    Xmoy = np.mean(VR[np.where(SI == min(SI))])  # Abcisse du SI minimum
+
+    params0 = [0.2, Xmoy, 5.0, 1.0]  # Estimation initiale des paramètres de la gaussienne
+
+    popt, pcov = curve_fit(gaus, VR, SI,
+                           params0)  # popt[]:paramètres optimisés; popt[0]: ordonnée moyenne ,popt[1]: abscisse
+    # moyenne, popt[2]: écart-type, popt[3]: continuum
+
     """#Graphique
     
     x = np.arange(-243,243, 0.2)
@@ -87,54 +83,63 @@ for filename in liste:
     plt.plot(VR,SI,'k.',label='donnees')
     plt.plot(x,y,'r-',label='lissage')
     plt.show()"""
-            
-# Borne inferieure et supérieure d'intégration (+/- 3 sigma)
-    binf = popt[1]-3*popt[2]
-    bsup = popt[1]+3*popt[2]
-    
+
+    # Borne inferieure et supérieure d'intégration (+/- 3 sigma)
+    binf = popt[1] - 3 * popt[2]
+    bsup = popt[1] + 3 * popt[2]
+
+    # Création d'un array des index du domaine d'intégration
     if binf > -243 and bsup < 243:
-# Créer un array des index du domaine d'intégration 
         bornesVR = ([np.where((VR >= binf) & (VR <= bsup))])
         domaine = np.array(bornesVR)
 
-# Données à intégrer
+        # Données à intégrer
         bVR = VR[domaine]
-        bSI = popt[3]-SI[domaine] #Ic-I(vr)
-        bSV = bVR*SV[domaine] #vr*V(vr)
+        bSI = popt[3] - SI[domaine]  # continuum - SI(VR)
+        bSV = bVR * SV[domaine]  # VR*V(VR)
         bSN = SN[domaine]
-            
-# Intégration
-        iSI = integ(bSI,bVR)
-        iSV = integ(bSV,bVR)
-        iSN = integ(bSN,bVR)
-        
-# Champ longitudinal
-        Bl = float(coeff*(iSV/iSI))
 
-# Calcul de l'erreur
+        # Intégration (par la loi de Simpson)
+        iSI = integ(bSI, bVR)
+        iSV = integ(bSV, bVR)
+        iSN = integ(bSN, bVR)
+
+        # Champ longitudinal
+        Bl = float(coeff * (iSV / iSI))
+
+        """# Paramètres de Stokes N (minimum et maximum de fit gaussiens
+        params0_N = [0.2, Xmoy, 5.0, 1.0]
+        popt_N, pcov_N = curve_fit(gaus, bVR, bSV, params0_N)
+
+        x = np.arange(binf, bsup, 0.1)
+        y = gaus(x, popt_N[0], popt_N[1], popt_N[2], popt_N[3])
+
+        import matplotlib.pyplot as plt
+
+        plt.plot(VR, SV, 'k.', label='donnees')
+        plt.plot(x, y, 'r-', label='lissage')
+        plt.show()"""
+
+        # Calcul de l'erreur
         ebSI = eSI[domaine]
-        ebSV = eSV[domaine] 
-        ebSN = eSN[domaine]
+        ebSV = eSV[domaine]
 
-        eiSI = integ(ebSI,bVR)
-        eiSV = integ(ebSV,bVR)
-        eiSN = integ(ebSN,bVR)
-        
-        erSV = ((coeff/iSI)*(eiSV**2))**2 
-        erSI = ((coeff*iSV/(iSI**2))*(eiSI**2))**2
-        erreur = float(np.sqrt(erSV+erSI))
+        eiSI = integ(ebSI, bVR)
+        eiSV = integ(ebSV, bVR)
 
-        imin = np.argmin(SV[domaine].tolist()) # Indice du SV minimum et maximum
-        imax = np.argmax(SV[domaine].tolist())
-#        Xmin = bVR[imin]
-#        imean = np.mean(SV[domaine].tolist())
-	print(bVR)
+        erSV = ((coeff / iSI) * (eiSV ** 2)) ** 2
+        erSI = ((coeff * iSV / (iSI ** 2)) * (eiSI ** 2)) ** 2
+        erreur = float(np.sqrt(erSV + erSI)) + abs(iSN)
 
-        compteur = compteur+1
+    	print filename, '\t', "%.f" % Bl,'\t', "%.f" %  erreur
 
-#        print(filename, "%.f" % Bl, "%.f" %  erreur, "%.f" %  popt[1],	"%.4f" %  popt[2],	"%.4f" %  popt[3],	"%.4f" %  (popt[3]-popt[0]))
-#        print filename, '\t', "%.f" % Bl,'\t', "%.f" %  erreur,'\t', "%.f" %  popt[1],'\t',	"%.4f" %  popt[2],'\t',	"%.4f" %  popt[3],'\t',	"%.4f" %  (popt[3]-popt[0])
+    #        print(filename, "%.f" % Bl, "%.f" %  erreur, "%.f" %  popt[1],	"%.4f" %  popt[2],	"%.4f" %  popt[3],	"%.4f" %  (popt[3]-popt[0]))
+    #        print filename, '\t', "%.f" % Bl,'\t', "%.f" %  erreur,'\t', "%.f" %  popt[1],'\t',	"%.4f" %  popt[2],'\t',	"%.4f" %  popt[3],'\t',	"%.4f" %  (popt[3]-popt[0])
     else:
         atrocites.append(filename)
 
-# print(len(liste), compteur, atrocites)
+"""        imin = np.argmin(SV[domaine].tolist()) # Indice du SV minimum et maximum
+        imax = np.argmax(SV[domaine].tolist())
+        Xmin = bVR[imin]
+        imean = np.mean(SV[domaine].tolist())"""
+# print(len(liste), atrocites)
